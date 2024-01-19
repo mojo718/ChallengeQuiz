90141a7bce1a490477fd20c8c46e9635d49beae5
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-01-19 18:54:35 -0500

    Starting Challenge4 - Just getting folders, html, JS script, and css files setup.  Did some psuedo coding

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// psuedo coding things i need to do:
+// some HTML and CSS to display quiz
+// creating global Vars with Arrays for questions, choices and answers
+// functions:
+// function start quiz with a timer
+// function show the questions using a Carousel (thanks Bobby for help with this!)
+// function to check the answers
+// function to update the timer
+// function to end quiz 
+// function to recored the score (think we need json here)
+// function to display score (think we need json here)
+// function to display score in a different Window
+
